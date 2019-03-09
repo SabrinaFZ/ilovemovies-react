37e4f3e6db880b260484ad33a15b35f3f50a6872
SabrinaFZ <sabrina.fer.zam@gmail.com>
2019-03-09 17:24:03 +0100

    Remove ratings in movie in home page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,7 +9,7 @@ class Movie extends React.Component {
     render(){
         return (
             <section className="movie-section">
-                <Ratings />
+                {/* <Ratings /> */}
                 <div className="movie-section_info">
                     <img className="movie-section_poster" src={`https://image.tmdb.org/t/p/w500/${this.props.movie.poster_path}`} alt={`${this.props.movie.title} Poster`} />
                     <p className="movie-section_title">{this.props.movie.title}</p>
