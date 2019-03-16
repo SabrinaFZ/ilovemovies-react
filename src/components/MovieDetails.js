6e237d9530f7c215893b0f6da65016b1b11d35d9
SabrinaFZ <sabrina.fer.zam@gmail.com>
2019-03-16 17:11:53 +0100

    Modify styles and add more info in movie details

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -11,17 +11,26 @@ class MovieDetails extends React.Component {
       movie: {}
     };
   }
-  
-  async componentDidMount() {
-    const SEARCH_MOVIE_URL = `https://api.themoviedb.org/3/movie/${
-      this.props.match.params.id
-    }?api_key=e53ed30d9e273053803f465b52b55158&language=en-US`;
 
-    let response = await fetch(SEARCH_MOVIE_URL);
-    let responseJSON = await response.json();
+  async componentDidMount() {
+    const URL_BASE = `https://api.themoviedb.org/3/movie/${
+      this.props.match.params.id
+    }`;
+    const SEARCH_MOVIE_URL = `${URL_BASE}?api_key=e53ed30d9e273053803f465b52b55158&language=en-US`;
+    const SEARCH_CAST_MOVIE_URL = `${URL_BASE}/credits?api_key=e53ed30d9e273053803f465b52b55158`;
+
+    let movieDetails = await this.callAPI(SEARCH_MOVIE_URL);
+    let cast = await this.callAPI(SEARCH_CAST_MOVIE_URL);
+
     this.setState({
-      movie: responseJSON
+      movie: { ...movieDetails, ...cast}
     });
+  }
+
+  async callAPI(URL){
+    let response = await fetch(URL);
+    let responseJSON = await response.json();
+    return responseJSON;
   }
 
   render() {
@@ -29,25 +38,44 @@ class MovieDetails extends React.Component {
 
     return (
       <main className="movie-info">
-      <div>
-                <img
-                    className="movie-info_poster"
-                    src={
-                        movie.poster_path
-                            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-                            : default_poster
-                    }
-                    alt={`${movie.title} Poster`}
-                />
-      </div>
-        
+        <div>
+          <img
+            className="movie-info_poster"
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : default_poster
+            }
+            alt={`${movie.title} Poster`}
+          />
+        </div>
+
         <div className="movie-info_info">
           <ul>
-            <li>
-              <p className="movie-info_title">{movie.title}</p>
+            <li className="movie-info_title">
+              <p>{movie.title}</p>
             </li>
-            <li>
-              <p className="movie-info_desc">{movie.overview}</p>
+            <li className="movie-info_desc">
+              <p>{movie.overview}</p>
+            </li>
+            <li className="movie-info_cast">
+              <h3>Cast:</h3>
+              {movie.cast &&
+                movie.cast.slice(0, 3).map((character, index) => {
+                  return (
+                    <span key={character.id}>
+                      {character.name}
+                      {index !== 2 ? "," : ""}{" "}
+                    </span>
+                  );
+                })}
+            </li>
+            <li className="movie-info_director">
+              <h3>Director: </h3> <p> {movie.crew && movie.crew[0].name}</p>
+            </li>
+            <li className="movie-info_release_date">
+              <h3>Release Date: </h3>
+              <p>{movie.release_date}</p>
             </li>
           </ul>
         </div>
