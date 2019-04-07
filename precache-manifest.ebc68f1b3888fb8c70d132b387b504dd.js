2c43c235b0fbf2b84ac94aca5560f5a45cffd04c
SabrinaFZ <sabrina.fer.zam@gmail.com>
2019-04-07 17:52:59 +0200

    Updates

---
+++ precache-manifest.ebc68f1b3888fb8c70d132b387b504dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcf5e9d44fa8e40a5d320763dc862453",
+    "url": "/ilovemovies-react/static/media/movie_default.dcf5e9d4.png"
+  },
+  {
+    "revision": "ecfaa44c30e174048682",
+    "url": "/ilovemovies-react/static/js/runtime~main.c88da309.js"
+  },
+  {
+    "revision": "1caf1125d8a16e97d491",
+    "url": "/ilovemovies-react/static/js/main.81eb4d36.chunk.js"
+  },
+  {
+    "revision": "3d364e1a18b439788e96",
+    "url": "/ilovemovies-react/static/js/2.30c52a9a.chunk.js"
+  },
+  {
+    "revision": "1caf1125d8a16e97d491",
+    "url": "/ilovemovies-react/static/css/main.0c560642.chunk.css"
+  },
+  {
+    "revision": "938e6dae20098d3360c66f87ded0a1c9",
+    "url": "/ilovemovies-react/index.html"
+  }
+];
